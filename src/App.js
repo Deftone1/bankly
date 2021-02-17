c0a39e5cfab4b247cf05fe9498c0878afd226fc5
Paul Murray <paulmurray85@gmail.com>
2021-02-17 11:43:32 -0800

    sidebar styling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div>
       <Router>
-
-      <Navbar />
+        <Sidebar />
+        <Navbar />
       </Router>
     </div>
   );
