4749552f870730a8b99b6d7174699eb58a18d68f
Paul Murray <paulmurray85@gmail.com>
2021-02-18 10:35:19 -0800

    testing out github linguist

---
+++ src/components/InfoSection/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  InfoContainer,
+  InfoWrapper,
+  InfoRow,
+  Column1,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+  BtnWrap,
+  Button,
+} from "./InfoElements";
+
+const InfoSection = () => {
+  return (
+    <InfoContainer id={id}>
+      <InfoWrapper>
+        <InfoRow>
+          <Column1>
+            <TextWrapper>
+              <TopLine>Top Line</TopLine>
+              <Heading>Heading</Heading>
+              <Subtitle>Subtitle</Subtitle>
+              <BtnWrap>
+                <Button to="home">Button</Button>
+              </BtnWrap>
+            </TextWrapper>
+          </Column1>
+        </InfoRow>
+      </InfoWrapper>
+    </InfoContainer>
+  );
+};
+
+export default InfoSection;
